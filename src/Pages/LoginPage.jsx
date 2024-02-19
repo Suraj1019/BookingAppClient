a96482ba35bb3fac5f4e43fddb9b936071a0241b
Suraj1019 <surajgautam3110@gmail.com>
2024-02-19 19:58:21 +0530

    binded login and register api

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,20 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Login } from "../apiCalls";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
-    return (
-        <div className='mt-4 grow flex items-center justify-around'>
-            <div className='mb-12'>
-                <h1 className='text-4xl text-center mb-4 font-semibold'>Login</h1>
-                <form className='max-w-md mx-auto'>
-                    <input type="email" placeholder='your@email.com' />
-                    <input type="password" placeholder='password' />
-                    <button className='primary'>Login</button>
-                    <div className='text-center text-gray-500 py-2'>Don't have an account yet? <Link to={'/register'} className='underline text-black'>Register now</Link></div>
-                </form>
-            </div>
-        </div>
-    )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-export default LoginPage
+  const login = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await Login(email, password);
+      console.log(response);
+      alert("Login Sucessfull");
+      navigate("/");
+    } catch (error) {
+      alert("Login Failed");
+    }
+  };
+  return (
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-12">
+        <h1 className="text-4xl text-center mb-4 font-semibold">Login</h1>
+        <form className="max-w-md mx-auto" onSubmit={login}>
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="primary">Login</button>
+          <div className="text-center text-gray-500 py-2">
+            Don't have an account yet?{" "}
+            <Link to={"/register"} className="underline text-black">
+              Register now
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
